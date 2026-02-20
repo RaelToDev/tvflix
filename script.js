e6560edae59c518d8881bf823312403dc9e7d4c1
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2026-02-19 22:09:31 -0300

    Criando o card de exemplo

---
+++ script.js
@@ -0,0 +1,13 @@
+fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGU3MDFkYjNmNTUyZTBhNTFjMDlkNDMxMzdiZDI3MCIsIm5iZiI6MTY4ODczMDA1NC44NzgsInN1YiI6IjY0YTdmOWM2OTY1MjIwMDExZGYwOGU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YhX8YDb0OF8ovacEzdWjUTSWr0xZLaZOItyxsnzgVMI'
+    }
+})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.results)
+    })
