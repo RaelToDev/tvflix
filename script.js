8c915d01bb38bc197439b992a6bec85a308a997f
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2026-02-26 18:57:56 -0300

    Ajuste no card para exemplo dentro do html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', {
-    method: 'GET',
+    //method: 'GET',
     headers: {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGU3MDFkYjNmNTUyZTBhNTFjMDlkNDMxMzdiZDI3MCIsIm5iZiI6MTY4ODczMDA1NC44NzgsInN1YiI6IjY0YTdmOWM2OTY1MjIwMDExZGYwOGU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YhX8YDb0OF8ovacEzdWjUTSWr0xZLaZOItyxsnzgVMI'
